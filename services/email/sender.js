21cb9c3d3b4b3f2d303d8d178cb0d7fffeabbd3f
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-11-02 19:20:43 +0200

    added port,host,email to env

--- services/email/sender.js
+++ services/email/sender.js
@@ -1,23 +1,26 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-
+const SENDER_HOST = process.env.SENDER_HOST;
+const SENDER_PORT = process.env.SENDER_PORT;
 const PASSWORD = process.env.PASSWORD;
+const EMAIL = process.env.EMAIL;
+
 class CreateSender {
     async send(message) {
         const config = {
-            host: 'smtp.meta.ua',
-            port: 465,
+            host: SENDER_HOST,
+            port: SENDER_PORT,
             secure: true,
             auth: {
-                user: 'v.rzhanov@meta.ua',
+                user: EMAIL,
                 pass: PASSWORD,
             },
         };
         
         const transporter = nodemailer.createTransport(config);
         try {
-            const result = await transporter.sendMail({ ...message, from: 'v.rzhanov@meta.ua' });
+            const result = await transporter.sendMail({ ...message, from: EMAIL });
         return result
         } catch (error) {
             console.log(error);
